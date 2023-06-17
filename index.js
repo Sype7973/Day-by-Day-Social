d5d97168e3b9d5ad270e6a20b8e30f657d60e728
unknown <adam.day7973@gmail.com>
2023-06-17 13:33:23 +1000

    updated index.js server page

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const cwd = process.cwd();
 const PORT = process.env.port || 8080;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
+// // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+// const activity = cwd.includes('/Day-by-Day-Social/')
+//   ? cwd.split('/Day-by-Day-Social/')[1]
+//   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
